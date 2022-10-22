ccc3e0db58dbb21f2fa5cbe542bb512eb3fb15ae
Rodion Shpak <98263512+RodionShpak@users.noreply.github.com>
2022-10-22 14:17:32 +0300

    fp

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
-import { useState  } from 'react';
-import { Bar,SearchForm ,Button,Input} from './Searchbar.styled';
+import { useState } from 'react';
+import { Bar, SearchForm, Button, Input } from './Searchbar.styled';
 
 
 export const Searchbar = (props) => {
@@ -9,30 +9,30 @@ export const Searchbar = (props) => {
   const [query, setQuery] = useState('');
 
   const handleChange = (e) => {
-    setQuery(e.currentTarget.value );
+    setQuery(e.currentTarget.value);
   };
 
   const handleSubmit = e => {
     e.preventDefault();
     props.onSubmit(query);
-    };
+  };
 
 
-    return (
-      <Bar>
+  return (
+    <Bar>
       <SearchForm onSubmit={handleSubmit}>
         <Input
           type="text"
           value={query}
           onChange={handleChange}
         />
-        <Button type="submit">Искать</Button>
+        <Button type="submit">Шукати</Button>
       </SearchForm>
-      </Bar>
-    );
+    </Bar>
+  );
 
 }
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired
-  }
+}
